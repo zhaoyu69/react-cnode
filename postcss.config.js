1c5ddb0c1964578a477b79e0f9bff5016f9b4a1b
zysoft <zhaoyu_m69@163.com>
2018-09-12 17:23:27 +0800

    add flexiable px2rem

--- postcss.config.js
+++ postcss.config.js
@@ -5,6 +5,10 @@ module.exports = {
                 // 加这个后可以出现额外的兼容性前缀
                 "> 0.01%"
             ]
+        }),
+        require('postcss-px2rem')({
+            remUnit: 75,
+            threeVersion: true
         })
     ]
 };
